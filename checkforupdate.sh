90ffc509553ca71bf8918b125b62c30fafa17ffd
Andrew Teixeira <teixeira@broadinstitute.org>
2015-07-29 17:11:02 -0400

    Add checkforupdate.sh

---
+++ checkforupdate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd -P "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+source "${SCRIPT_DIR}/config.sh"
+
+if [ "$TERM" != "dumb" ];
+then
+    TTY='-it'
+fi
+
+if [ ! -w "${DOCKER_SOCKET}" ];
+then
+    SUDO='sudo'
+fi
+
+$SUDO docker run $TTY --rm \
+       $GADS_IMAGE \
+       /gads/checkforupdate
