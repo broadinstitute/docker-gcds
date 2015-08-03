99ea7412ab9d7de2c1e17a9f131a4383ade8ab5e
Andrew Teixeira <teixeira@broadinstitute.org>
2015-08-03 16:55:08 -0400

    * Add to .gitignore * Add config.sample.py as an example for how to make a config.py for GADSrunner

---
+++ config.sample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+debug = True
+
+google_ldap_new_prod = {
+    'ldap_uri': 'ldap://ldap.example.org',
+    'ldap_base_dn': 'ou=theou,dc=example,dc=org',
+    'ldap_bind_dn': 'cn=Manager,dc=example,dc=org',
+    'ldap_bind_pw': 'secretpassword',
+    'ldap_people_ou': 'ou=someou,ou=theou,dc=example,dc=org',
+}
